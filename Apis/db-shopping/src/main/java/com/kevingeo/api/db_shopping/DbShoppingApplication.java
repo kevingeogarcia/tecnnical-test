f464c3fdd0235096c4072c2219f26cbd7f191fc1
Kevin Hernández <kevingeogarcia@gmail.com>
2025-02-23 04:40:45 -0600

    add api bd-shopping

---
+++ Apis/db-shopping/src/main/java/com/kevingeo/api/db_shopping/DbShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.kevingeo.api.db_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbShoppingApplication.class, args);
+	}
+
+}
---
+++ Apis/db-shopping/src/test/java/com/kevingeo/api/db_shopping/DbShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevingeo.api.db_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
